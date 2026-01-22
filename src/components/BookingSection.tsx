403f24e46239ee1e9eda7d0bdd5d8c3aa01a55e7
stefan-bouognong <stefanbouognong@gmail.com>
2026-01-22 08:16:54 +0100

    test number

--- src/components/BookingSection.tsx
+++ src/components/BookingSection.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { MessageCircle, Send, CheckCircle } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
-const WHATSAPP_NUMBER = "1 (301) 732-9975";
+const WHATSAPP_NUMBER = "1(301)732-9975";
 
 
 const services = [
